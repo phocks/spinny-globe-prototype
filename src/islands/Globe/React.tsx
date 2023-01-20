8f8fb05c164ffbce02e94644b5fccd3c0e654c1d
Joshua Byrd <phocks@gmail.com>
2023-01-20 17:16:17 +1000

    OK maybe dynamic imports are good afterall

--- src/islands/Globe/React.tsx
+++ src/islands/Globe/React.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useId } from "react";
 import { v5 as uuidv5 } from "uuid";
 
-import mountGlobe from "./mountGlobe";
-
 function Globe({}) {
   const id = useId(); // server-client consistent id
   const mountHash = `spinny-globe-mount-${uuidv5(id, uuidv5.URL)}`;
 
   useEffect(() => {
-    mountGlobe({ mountName: mountHash });
+    import("./mountGlobe").then(({ default: mountGlobe }) => {
+      mountGlobe({ mountName: mountHash });
+    });
   }, []);
 
   return <div className={mountHash}></div>;
